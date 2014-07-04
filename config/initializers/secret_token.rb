58d337f899427780f3834cc0ad645f495d4cf9a9
= <=>
2014-07-04 00:38:35 -0400

    Initial rails build of goal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goal::Application.config.secret_token = '1415e92353cf3d284d8eec763b34a99bdc84252e6190c93bb1b5098de133db4f8b358ebddf58c560e3c15e038d65855669bdcd982f1a722466938e99a19bd868'
